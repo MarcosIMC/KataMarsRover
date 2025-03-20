9e79e96fa70d31f03dfbde4a3de6cea95ffca471
Marcos Medina <mimc.software@gmail.com>
2025-03-20 15:37:30 +0000

    Change main

--- bin/mars_rover_kata.dart
+++ bin/mars_rover_kata.dart
@@ -1,40 +1,13 @@
-import 'dart:io';
+import 'package:mars_rover_kata/api.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
-import 'package:mars_rover_kata/mars_rover_kata.dart' as mars_rover_kata;
-import 'package:mars_rover_kata/models/Orientation.dart';
-import 'package:mars_rover_kata/models/commandValidator.dart';
-import 'package:mars_rover_kata/models/controlPanel.dart';
-import 'package:mars_rover_kata/models/mars.dart';
-import 'package:mars_rover_kata/models/rover.dart';
-import 'package:mars_rover_kata/models/roverCommand.dart';
-
-void main(List<String> arguments) {
-  final mars = Mars(10, 10);
-  final rover = Rover(0, 0, Orientation.NORTH, mars);
-  final controlPanel = ControlPanel();
-  final commandValidator = CommandValidator();
-
-  String? input = stdin.readLineSync();
-
-  if (commandValidator.isInvalidCommands(input!)) {
-    throw ArgumentError();
-  }
-
-  for(var command in input.split('')) {
-    switch(command) {
-      case 'M':
-        controlPanel.addCommand(MoveCommand(rover));
-        break;
-      case 'L':
-        controlPanel.addCommand(TurnLeftCommand(rover));
-        break;
-      case 'R':
-        controlPanel.addCommand(TurnRightCommand(rover));
-        break;
-    }
-  }
-
-  controlPanel.executeCommands();
-
-  print(rover.currentPosition());
+void main(List<String> arguments) async {
+  var handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+  var server = await shelf_io.serve(Api().handler, 'localhost', 8080);
+  server.autoCompress = true;
 }
+
+Response _echoRequest(Request request) =>
+    Response.ok('Request for "${request.url}"');
